fe85f5e4037cc4ec1d9f0c40545aeb69ab6c9e4b
cyj1127 <157556973+cyj1127@users.noreply.github.com>
2024-07-04 11:38:47 +0900

    fix: 맛집상세 리뷰아이템 import 수정

--- app/store/[store_id]/page.tsx
+++ app/store/[store_id]/page.tsx
@@ -1,7 +1,7 @@
 import "@/app/store/[store_id]/storeDetailPage.scss"
 import "@/app/store/[store_id]/storeDetailMap.scss"
 import StoreDetail from "@/app/store/[store_id]/component/StoreDetail";
-import { ReviewItem } from "@/app/(component)/reviewItem";
+import ReviewItem from "@/app/(component)/reviewItem";
 const Store = () => {
   return (
     <div className="StoreDetailComponent" style={{ display: "flex", justifyContent: "center", width: "100%", flexDirection: "column", alignItems: "center", margin: "0px" }}>
