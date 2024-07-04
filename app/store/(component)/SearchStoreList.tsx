d3ddaaac362de22ef46c3819af0c71203e033b0c
cyj1127 <157556973+cyj1127@users.noreply.github.com>
2024-07-04 11:15:24 +0900

    feat:즐겨찾기 기능 구현
    즐겨찾기 상태 저장 및 아이콘 색상 변경

--- app/store/(component)/SearchStoreList.tsx
+++ app/store/(component)/SearchStoreList.tsx
@@ -2,17 +2,16 @@ import TestMap from "@/app/store/(component)/NaverMap";
 import myLocation from "./MyLocation"
 import StoreList from "./StoreList";
 
-
 const SearchStoreList = () => {
-    return (
-        <div className="container">
-            <div className="content">
-                {/* 지도 Api  */}
-                <TestMap />
-                {/* 가게 리스트 */}
-                <StoreList />
-            </div>
-        </div>
-    )
+  return (
+    <div className="container">
+      <div className="content">
+        {/* 지도 Api  */}
+        <TestMap />
+        {/* 가게 리스트 */}
+        <StoreList />
+      </div>
+    </div>
+  )
 }
 export default SearchStoreList;
