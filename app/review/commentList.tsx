80cce79ae20d1a8c7268ac49b249d752b1e4a29d
kimsiun <157556686+kimsiun@users.noreply.github.com>
2024-07-05 11:41:23 +0900

    feat: 댓글 리스트 생성

--- app/review/commentList.tsx
+++ app/review/commentList.tsx
@@ -1,34 +1,29 @@
-import './commentList.scss';
+import "./commentList.scss";
 
 const CommentList = () => {
-    return (
-        <> 
-        <div className = 'commentContainer' >
+  return (
+    <>
+      <div className="commentContainer">
+        <div className="content">
+          <img src="" alt="프로필 이미지" className="comment-img" />
 
-<div className='content'>
-          <img src='' alt='프로필 이미지' className='comment-img' />
-
-          <div className='right'>
-              <p className='profile-name'>
-                  귀여운 닥스훈트
-              </p>
-              <p>맛있겠다 헐~ 쥐져스 크라이스트</p>
-              <p>2020.20.20</p>
+          <div className="right">
+            <p className="profile-name">귀여운 닥스훈트</p>
+            <p>맛있겠다 헐~ 쥐져스 크라이스트</p>
+            <p>2020.20.20</p>
           </div>
-          </div>
-
-<div className='btn'>
-        <button className='dec'></button>
-        <button className='mod'></button>
-        <button className='del'></button>
         </div>
-    </div>
 
-    <hr/>
+        <div className="btn">
+          <button className="dec"></button>
+          <button className="mod"></button>
+          <button className="del"></button>
+        </div>
+      </div>
 
-
-</>
-    );
-}
+      <hr />
+    </>
+  );
+};
 
 export default CommentList;
