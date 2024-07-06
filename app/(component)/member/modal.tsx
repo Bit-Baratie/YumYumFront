f26e37d87a9b06a87f99a7967a996f298afc21fe
dyddks <dyddks0115@naver.com>
2024-07-06 16:45:54 +0900

    feat: 회원정보수정 모달

--- app/(component)/member/modal.tsx
+++ app/(component)/member/modal.tsx
@@ -1,34 +1,75 @@
+import useMember from '@/app/(hooks)/member/useMember';
 import ModalStyle from './modal.module.scss';
+import { ChangeEvent } from 'react';
+import { useParams } from 'next/navigation';
 
 interface Props {
-  image: string;
+  imageHandler: (e: ChangeEvent<HTMLInputElement>) => void;
+  nickNameHandler: (e: ChangeEvent<HTMLInputElement>) => void;
+  passwordHanler: (e: ChangeEvent<HTMLInputElement>) => void;
+  passwordCheckHanler: (e: ChangeEvent<HTMLInputElement>) => void;
+  phoneHandler: (e: ChangeEvent<HTMLInputElement>) => void;
+  updateMember: () => void;
   nickName: string;
   phone: string;
+  setModal: (i:boolean) => void;
 }
 
-const Modal = ({image, nickName, phone}: Props) => {
+const Modal = (
+  {imageHandler,
+  nickNameHandler,
+  passwordHanler,
+  passwordCheckHanler,
+  phoneHandler,
+  updateMember,
+  nickName,
+  phone,
+  setModal}: Props
+) => {
+
   return (
-    <div className={ModalStyle.container}>
-      <div className={ModalStyle.esc}>X</div>
-      <span className={ModalStyle.title}>프로필 수정</span>
-      <div className={ModalStyle.img}></div>
-      <div><input type="text" defaultValue={nickName} className={ModalStyle.nickname} readOnly/><span>연필</span></div>
-      <div>
-        <div className={ModalStyle.inputSet}>
-          <span className={ModalStyle.label}>연락처</span>
-          <div className={ModalStyle.inputBox}><input type="text" defaultValue={phone} className={ModalStyle.input}/><span>연필</span></div>
+    <>
+      <div className={ModalStyle.background}></div>
+
+      <div className={ModalStyle.container}>
+        <div className={ModalStyle.esc} onClick={() => setModal(false)}>BC</div>
+        <span className={ModalStyle.title}>프로필 수정</span>
+        <div className={ModalStyle.img}></div>
+
+        <div>
+          <input type="text" value={nickName} className={ModalStyle.nickname} onChange={(e) => nickNameHandler(e)}/>
+          <span>연필</span>
         </div>
-        <div className={ModalStyle.inputSet}>
-          <span className={ModalStyle.label}>변경 비밀번호</span>
-          <div className={ModalStyle.inputBox}><input type="text" className={ModalStyle.input}/><span>연필</span></div>
+
+        <div>
+          <div className={ModalStyle.inputSet}>
+            <span className={ModalStyle.label}>연락처</span>
+            <div className={ModalStyle.inputBox}>
+              <input type="text" className={ModalStyle.input} onChange={(e) => phoneHandler(e)}/>
+              <span>연필</span>
+            </div>
+          </div>
+
+          <div className={ModalStyle.inputSet}>
+            <span className={ModalStyle.label}>변경 비밀번호</span>
+            <div className={ModalStyle.inputBox}>
+              <input type="text" className={ModalStyle.input} onChange={(e) => passwordHanler(e)}/>
+              <span>연필</span>
+            </div>
+          </div>
+
+          <div className={ModalStyle.inputSet}>
+            <span className={ModalStyle.label}>변경 비밀번호 확인</span>
+            <div className={ModalStyle.inputBox}>
+              <input type="text" className={ModalStyle.input} onChange={(e) => passwordCheckHanler(e)}/>
+              <span>연필</span>
+            </div>
+          </div>
         </div>
-        <div className={ModalStyle.inputSet}>
-          <span className={ModalStyle.label}>변경 비밀번호 확인</span>
-          <div className={ModalStyle.inputBox}><input type="text" className={ModalStyle.input}/><span>연필</span></div>
-        </div>
+
+        <button className={ModalStyle.submit} onClick={updateMember}>확인</button>
       </div>
-      <button className={ModalStyle.submit}>확인</button>
-    </div>
+    </>
   );
 }
 
