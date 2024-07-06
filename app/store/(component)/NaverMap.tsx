27e814b8ad3c049b20cdaad4e834de1c362648a3
cyj1127 <157556973+cyj1127@users.noreply.github.com>
2024-07-06 17:30:48 +0900

    feat:맛집 즐겨찾기
    백엔드와 데이터 통신 구현

--- app/store/(component)/NaverMap.tsx
+++ app/store/(component)/NaverMap.tsx
@@ -3,8 +3,9 @@ import '@/app/store/(component)/map.scss';
 import Location from '../../../public/asset/image/location.svg'
 import myLocation from "./MyLocation";
 import Script from "next/script";
-import { useEffect } from "react";
-import { getStoreInfo } from "@/app/store/(api)/StoreApi";
+import { useEffect, useRef } from "react";
+import useStoreApi from "@/app/store/(api)/StoreApi";
+import { initializeMap } from './mapInitializer';
 
 const TestMap = () => {
 
@@ -12,20 +13,20 @@ const TestMap = () => {
     latitude: number,
     longitude: number,
   }
+  const { getStoreInfo } = useStoreApi();
   let latitude = 37.359531;
   let longitude = 127.1052133;
   let map: naver.maps.Map;
 
-  const LatLng: location = { latitude, longitude };
 
+  const LatLng: location = { latitude, longitude };
 
   useEffect(() => {
     const MapRender = async () => {
       const result = await getStoreInfo(LatLng);
-
-
       const location = new naver.maps.LatLng(result[0].latitude, result[0].longtitude);
-      //지도 그리기
+
+
       map = new naver.maps.Map('map', {
         center: location,
         zoomControl: true,   // 줌 설정
@@ -36,6 +37,8 @@ const TestMap = () => {
           position: naver.maps.Position.TOP_RIGHT,
         },
       });
+      //지도 그리기
+
       result.map(store => {
         var marker = new naver.maps.Marker({
           position: new naver.maps.LatLng(store.latitude, store.longtitude),
@@ -43,8 +46,7 @@ const TestMap = () => {
           title: "Location Marker"
         });
       });
-
-      // myLocation(map);
+      myLocation(map);
     };
     MapRender();
   }, [])
@@ -55,7 +57,7 @@ const TestMap = () => {
   return (
     <>
       <div id="map"></div>
-      <Script strategy='beforeInteractive' src="https://openapi.map.naver.com/openapi/v3/maps.js?ncpClientId=nqz0uvnoe5" />
+      <Script strategy='beforeInteractive' src="https://openapi.map.naver.com/openapi/v3/maps.js?ncpClientId=nqz0uvnoe5?" onLoad={initializeMap} />
       <button onClick={() => myLocation(map)} className="myLocation">
         <Location width='23' height='23' fill='white' />
       </button>
