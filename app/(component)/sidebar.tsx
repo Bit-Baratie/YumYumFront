ac67fbe96cf2be013ad8d04b80f5417429b9dc64
dyddks <dyddks0115@naver.com>
2024-07-07 16:38:04 +0900

    chore: sidebar url경로에 따라 색상변경

--- app/(component)/sidebar.tsx
+++ app/(component)/sidebar.tsx
@@ -5,54 +5,46 @@ import Reply from '@/public/asset/image/user.svg'
 import Like from '@/public/asset/image/user.svg'
 import Bookmark from '@/public/asset/image/bookmark.svg'
 import sidebarStyle from './sidebar.module.scss';
-import { usePathname } from 'next/navigation'
+import { useParams, usePathname } from 'next/navigation'
 import Link from 'next/link'
 import { useEffect, useState } from 'react'
+import UserStore from '../(hooks)/userStore'
+import Lib from '@ant-design/icons'
 
 const Sidebar = () => {
-  // const pathname = usePathname();
-  // const dashRegex = '^/member/\d+/$';  // 각 경로에 대한 정규표현식
-  // const reviewRegex = /member+[/]+\d+[/]+review/;
-  // const replyRegex = /member+[/]+\d+[/]+reply/;
-  // const likeRegex = /member+[/]+\d+[/]+like/;
-  // const starRegex = /member+[/]+\d+[/]+star/;
-
-  const [active, setActive] = useState<number>(0);
-
-  const clickHandler = (i:number) => {
-    setActive(i);
-  }
+  const pathname = usePathname();
+  const {userInfo} = UserStore();
 
   return (
     <ul className={sidebarStyle.sidebar}>
-      <Link href={`/member/${1}`}>
-      <li onClick={() => clickHandler(0)} className={active===0?sidebarStyle.activeColor:sidebarStyle.nonActiveColor}>
-        <span className={active===0?sidebarStyle.active:sidebarStyle.nonActive}></span>
-        <User width='25' height='25' fill={active===0?'#ffb321':'black'}/>나의 정보</li>
+      <Link href={`/member/${userInfo.memberId}`}>
+      <li className={pathname===`/member/${userInfo.memberId}`?sidebarStyle.activeColor:sidebarStyle.nonActiveColor}>
+        <span className={pathname===`/member/${userInfo.memberId}`?sidebarStyle.active:sidebarStyle.nonActive}></span>
+        <User width='25' height='25' fill={pathname===`/member/${userInfo.memberId}`?'#ffb321':'black'}/>나의 정보</li>
       </Link>
 
-      <Link href={`/member/${1}/review`}>
-      <li onClick={() => clickHandler(1)} className={active===1?sidebarStyle.activeColor:sidebarStyle.nonActiveColor}>
-        <span className={active===1?sidebarStyle.active:sidebarStyle.nonActive}></span>
-        <Review width='25' height='25' fill={active===1?'#ffb321':'black'}/> 내가 쓴 리뷰</li>
+      <Link href={`/member/${userInfo.memberId}/review`}>
+      <li className={pathname===`/member/${userInfo.memberId}/review`?sidebarStyle.activeColor:sidebarStyle.nonActiveColor}>
+        <span className={pathname===`/member/${userInfo.memberId}/review`?sidebarStyle.active:sidebarStyle.nonActive}></span>
+        <Review width='25' height='25' fill={pathname===`/member/${userInfo.memberId}/review`?'#ffb321':'black'}/> 내가 쓴 리뷰</li>
       </Link>
 
-      <Link href={`/member/${1}/reply`}>
-      <li onClick={() => clickHandler(2)} className={active===2?sidebarStyle.activeColor:sidebarStyle.nonActiveColor}>
-        <span className={active===2?sidebarStyle.active:sidebarStyle.nonActive}></span>
-        <Reply width='25' height='25' fill={active===2?'#ffb321':'black'}/> 내가 쓴 댓글</li>
+      <Link href={`/member/${userInfo.memberId}/reply`}>
+      <li className={pathname===`/member/${userInfo.memberId}/reply`?sidebarStyle.activeColor:sidebarStyle.nonActiveColor}>
+        <span className={pathname===`/member/${userInfo.memberId}/reply`?sidebarStyle.active:sidebarStyle.nonActive}></span>
+        <Reply width='25' height='25' fill={pathname===`/member/${userInfo.memberId}/reply`?'#ffb321':'black'}/> 내가 쓴 댓글</li>
       </Link>
 
-      <Link href={`/member/${1}/like`}>
-      <li onClick={() => clickHandler(3)} className={active===3?sidebarStyle.activeColor:sidebarStyle.nonActiveColor}>
-        <span className={active===3?sidebarStyle.active:sidebarStyle.nonActive}></span>
-        <Like width='25' height='25' fill={active===3?'#ffb321':'black'}/> 좋아요 한 리뷰</li>
+      <Link href={`/member/${userInfo.memberId}/like`}>
+      <li className={pathname===`/member/${userInfo.memberId}/like`?sidebarStyle.activeColor:sidebarStyle.nonActiveColor}>
+        <span className={pathname===`/member/${userInfo.memberId}/like`?sidebarStyle.active:sidebarStyle.nonActive}></span>
+        <Like width='25' height='25' fill={pathname===`/member/${userInfo.memberId}/like`?'#ffb321':'black'}/> 좋아요 한 리뷰</li>
       </Link>
       
-      <Link href={`/member/${1}/star`}>
-      <li onClick={() => clickHandler(4)} className={active===4?sidebarStyle.activeColor:sidebarStyle.nonActiveColor}>
-        <span className={active===4?sidebarStyle.active:sidebarStyle.nonActive}></span>
-        <Bookmark width='25' height='25' fill={active===4?'#ffb321':'black'}/> 즐겨찾기한 맛집</li>
+      <Link href={`/member/${userInfo.memberId}/star`}>
+      <li className={pathname===`/member/${userInfo.memberId}/star`?sidebarStyle.activeColor:sidebarStyle.nonActiveColor}>
+        <span className={pathname===`/member/${userInfo.memberId}/star`?sidebarStyle.active:sidebarStyle.nonActive}></span>
+        <Bookmark width='25' height='25' fill={pathname===`/member/${userInfo.memberId}/star`?'#ffb321':'black'}/> 즐겨찾기한 맛집</li>
       </Link>
     </ul>
   );
