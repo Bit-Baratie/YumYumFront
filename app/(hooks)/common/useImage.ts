63796ac0510a59d18faea3c59b686f9840ffca59
dyddks <dyddks0115@naver.com>
2024-07-09 16:59:00 +0900

    feat: useImage

---
+++ app/(hooks)/common/useImage.ts
@@ -0,0 +1,38 @@
+import { ChangeEvent, useState } from "react";
+
+const useImage = () => {
+  const [image, setImage] = useState<File[]>([]);
+  const [preview, setPreview] = useState<any>([]);
+
+  const imageHandler = (e:ChangeEvent<HTMLInputElement>) => {
+    if (e.target.files) {
+      const newImages = [...image];
+      const newPreviews = [...preview];
+
+      for (let i = 0; i < e.target.files!.length; i++) {
+        const file = e.target.files![i];
+        // 이벤트객체의 파일을 newImages에 담기
+        newImages.push(file);
+        // 파일리더 객체 생성
+        const reader = new FileReader();
+        // 파일 읽어온 후 실행되는 콜백함수
+        reader.onload = (e) => {
+          // 읽어온 값을 갱신하기
+          newPreviews.push(e.target!.result as string);
+          setPreview(newPreviews);
+        };
+        // 파일 객체를 읽어 base64 형태의 문자열로 변환
+        reader.readAsDataURL(file);
+      }
+      setImage(newImages);
+    }
+  }
+
+  return {
+    image,
+    preview,
+    imageHandler
+  }
+}
+
+export default useImage;
