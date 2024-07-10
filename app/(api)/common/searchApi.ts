8ba57dddcb886d94dae8184b552f1b817d06c33b
dyddks <dyddks0115@naver.com>
2024-07-10 18:17:36 +0900

    style: 불필요한 라인 삭제

--- app/(api)/common/searchApi.ts
+++ app/(api)/common/searchApi.ts
@@ -2,25 +2,6 @@ import axios from "axios"
 import useAxiosWithAuth from '@/app/(hooks)/common/useAxiosWithAuth';
 const { axiosWithAuth, axiosNonAuth } = useAxiosWithAuth();
 
-interface store {
-  storeId: number;
-  name: string;
-  address: string;
-  totalFavoriteCount: number;
-  totalReviewCount: number;
-  imageUrl: string;
-  views: number;
-  hashtags: hashtagListType[];
-  categoryName: string,
-  avgGrade: number;
-  favoriteStatus: boolean;
-}
-interface hashtagListType {
-  id: number,
-  content: string
-}
-
-
 export const search = async (keyword: string | null) => {
   const result = await axiosWithAuth.get(`/store/search/${keyword}`)
   console.log(result.data);
