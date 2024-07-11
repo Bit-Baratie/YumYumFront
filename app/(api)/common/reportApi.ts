36b4090f66d6b0231c1cd83711ff01e60ec54967
dyddks <dyddks0115@naver.com>
2024-07-11 15:33:15 +0900

    fix: 신고 ㅁ수정

--- app/(api)/common/reportApi.ts
+++ app/(api)/common/reportApi.ts
@@ -4,23 +4,13 @@ import { reportType } from "@/app/type";
 const reportApi = () => {
   const {axiosWithAuth} = useAxiosWithAuth();
 
-  const reviewReport = async (reportData: reportType) => {
+  const postReport = async (reportData: reportType) => {
     const result = await axiosWithAuth.post(`/report`, reportData );
     return result;
   }
 
-  const replyReport = (reportData: reportType) => {
-    
-  }
-
-  const storeReport = (reportData: reportType) => {
-    
-  }
-
   return {
-    reviewReport,
-    replyReport,
-    storeReport
+    postReport
   };
 }
 
