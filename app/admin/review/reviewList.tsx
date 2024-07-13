ddfe4456d7238faddfb16a2af09dac365ea52df0
kimsiun <157556686+kimsiun@users.noreply.github.com>
2024-07-13 16:53:28 +0900

    feat: 관리자 리뷰 관리 페이지

--- app/admin/review/reviewList.tsx
+++ app/admin/review/reviewList.tsx
@@ -1,15 +1,68 @@
-import "./reviewList.scss";
+"use client";
 
-const ReviewList = () => {
-  return ( 
+import React, { useState } from "react";
+import { useEffect } from "react";
+import { report } from "process";
+import AdminApi from "@/app/(api)/admin/adminApi";
+import ReviewStyle from "@/app/admin/review/reviewList.module.scss";
+import { CloseOutlined } from "@ant-design/icons";
+import Pagination from "react-js-pagination";
+import PageStyle from "@/app/admin/pageStyle.module.scss";
 
-    <tr className="list">
-        <td className="nickname">권재원</td>
-        <td className="reviewContent">너 기억력 어떻게 된거야??너 기억력 어떻게 된거야?? 너 기억력 어떻게 된거야??너 기억력 어떻게 된거야??</td>
-        <td className="reportContent">불쾌감</td> 
-        <td className="date"> 2020.20.20 <button>X</button> </td>
-    </tr>
-  );
+interface ReportData {
+  reportId: number;
+  nickName: string;
+  reportContents: string;
+  reportReason: string;
+  createdAt: string;
+  targetContent: string;
 }
+
+const ReviewList = ({ reportData }: { reportData: ReportData }) => {
+  const [page, setPage] = useState(1);
+
+  const handlePageChange = (page: number) => {
+    setPage(page);
+  };
+
+  // const [reportContents, setReportContents] = useState<any>();
+  // const { getReviewReport } = AdminApi();
+
+  // const fetchReviewReport = async () => {
+  //   const result = await getReviewReport();
+  //   setReportContents(result.content);
+  // };
+
+  // useEffect(() => {
+  //   fetchReviewReport();
+  // }, []);
+
+  return (
+    <>
+      {reportData && (
+        <tr className={ReviewStyle.trStyle}>
+          <td className={ReviewStyle.nickname}>
+            {reportData?.reportId}
+            {reportData?.nickName}
+          </td>
+          <td className={ReviewStyle.reviewContent}>
+            {reportData?.targetContent}
+            랄랄ㄹ랄라라라라@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@
+          </td>
+          <td className={ReviewStyle.reportContent}>
+            {reportData?.reportReason}
+            @@@@@@@
+          </td>
+          <td className={ReviewStyle.data}>
+            {reportData?.createdAt}
+            <button className={ReviewStyle.BtnStyle}>
+              <CloseOutlined />
+            </button>
+          </td>
+        </tr>
+      )}
+    </>
+  );
+};
 
 export default ReviewList;
