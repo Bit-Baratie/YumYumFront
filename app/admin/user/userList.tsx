874ca16bcb13f29267dbb53adb5b7406c23f4e1d
kimsiun <157556686+kimsiun@users.noreply.github.com>
2024-07-15 14:37:15 +0900

    feat: 관리자 페이지 사용자 목록 Api 생성

--- app/admin/user/userList.tsx
+++ app/admin/user/userList.tsx
@@ -3,32 +3,53 @@
 import { useEffect, useState } from "react";
 import adminApi from "@/app/(api)/admin/adminApi";
 import UserStyle from "@/app/admin/user/userList.module.scss";
+import useMember from "@/app/(hooks)/member/useMember";
 
-const UserList = () => {
-  const [reportContent, setReportContent] = useState();
-  const { getUserReport } = adminApi();
+interface ReportData {
+  reportId: number;
+  nickName: string;
+  reportContents: string;
+  reportReason: string;
+  createdAt: string;
+  targetContent: string;
+}
 
-  const fetchUserReport = async () => {
-    const result = await getUserReport();
-    setReportContent(result.content);
+const UserList = ({ reportData }: { reportData: ReportData }) => {
+  const [page, setPage] = useState(1);
+
+  const handlePageChange = (page: number) => {
+    setPage(page);
   };
 
-  useEffect(() => {
-    fetchUserReport();
-  }, []);
+  const { removeMember } = useMember();
+  // const [reportContent, setReportContent] = useState();
+  // const { getUserReport } = adminApi();
+
+  // const fetchUserReport = async () => {
+  //   const result = await getUserReport();
+  //   setReportContent(result.content);
+  // };
+
+  // useEffect(() => {
+  //   fetchUserReport();
+  // }, []);
 
   return (
-    <tr className={UserStyle.trStyle}>
-      <td className={UserStyle.Unikname}>
-        {/* {reportContent[0]?.nickname} */}
-        꼬마돌
-      </td>
-      <td className={UserStyle.email}>ddd@naver.com</td>
-      <td className={UserStyle.userNumbe}>010-1234-5678</td>
-      <td className={UserStyle.date}>
-        상태 <button className={UserStyle.UBtnStyle}>회원탈퇴</button>{" "}
-      </td>
-    </tr>
+    <>
+      {reportData && (
+        <tr className={UserStyle.trStyle}>
+          <td className={UserStyle.Unikname}>{reportData?.reportId}꼬마돌</td>
+          <td className={UserStyle.email}>{}ddd@naver.com</td>
+          <td className={UserStyle.userNumbe}>010-1234-5678</td>
+          <td className={UserStyle.date}>
+            상태{" "}
+            <button className={UserStyle.UBtnStyle} onClick={removeMember}>
+              회원탈퇴
+            </button>{" "}
+          </td>
+        </tr>
+      )}
+    </>
   );
 };
 
