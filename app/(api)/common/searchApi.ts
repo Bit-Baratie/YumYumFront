01badfbf2648c9db176daed060f2eb552fa680bd
dyddks <dyddks0115@naver.com>
2024-07-17 18:47:56 +0900

    fix: axios instance 수정

--- app/(api)/common/searchApi.ts
+++ app/(api)/common/searchApi.ts
@@ -1,7 +1,6 @@
-import useAxiosWithAuth from '@/app/(hooks)/common/useAxiosWithAuth';
+import { axiosWithAuth } from "@/app/(hooks)/common/useAxiosWithAuth";
 
 export const search = async (keyword: string | null) => {
-  const { axiosWithAuth } = useAxiosWithAuth();
   // if (!keyword) return false;
   const result = await axiosWithAuth.get(`/search/${keyword}`);
   console.log(result);
--- app/(hooks)/common/useAxiosWithAuth.ts
+++ app/(hooks)/common/useAxiosWithAuth.ts
@@ -3,43 +3,38 @@ import axios, { AxiosInstance } from 'axios';
 import Store from '../userStore';
 import { redirect } from 'next/navigation';
 
-const useAxiosWithAuth = (): any => {
-  const axiosWithAuth: AxiosInstance = axios.create({
-    baseURL: 'http://223.130.158.171:80',
-    headers: {
-      Authorization: `Bearer ${Store.getState().token.atk}`,
-      "Content-Type": 'application/json'
-    }
- 
-  });
 
-  const axiosNonAuth: AxiosInstance = axios.create({
-    baseURL: 'http://223.130.158.171:80'
-  });
+export const axiosWithAuth: AxiosInstance = axios.create({
+  baseURL: 'http://223.130.158.171:80',
+  headers: {
+    Authorization: `Bearer ${Store.getState().token.atk}`,
+    "Content-Type": 'application/json'
+  }
 
-  axiosWithAuth.interceptors.response.use(
-    (response) => {
-      if (response.status === 401) {
-        redirect('/member/login');
-      } else {
-        return response;
-      }
-    },
-    (error) => {
-      console.error(error.message);
-    }
-  );
+});
 
-  axiosNonAuth.interceptors.response.use(
-    (response) => {
+export const axiosNonAuth: AxiosInstance = axios.create({
+  baseURL: 'http://223.130.158.171:80'
+});
+
+axiosWithAuth.interceptors.response.use(
+  (response) => {
+    if (response.status === 401) {
+      redirect('/member/login');
+    } else {
       return response;
-    },
-    (error) => {
-      console.error(error.message);
     }
-  );
+  },
+  (error) => {
+    console.error(error.message);
+  }
+);
 
-  return {axiosWithAuth, axiosNonAuth};
-};
-
-export default useAxiosWithAuth;
+axiosNonAuth.interceptors.response.use(
+  (response) => {
+    return response;
+  },
+  (error) => {
+    console.error(error.message);
+  }
+);
