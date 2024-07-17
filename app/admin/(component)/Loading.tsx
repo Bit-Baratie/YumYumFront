8b75e9c8fe31b9976b19a2c89cd8dfc123d44cf3
kimsiun <157556686+kimsiun@users.noreply.github.com>
2024-07-17 11:33:57 +0900

    feat: 로딩 컴포넌트 생성

---
+++ app/admin/(component)/Loading.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import Load from "./Loading.module.scss";
+
+const Loading = () => {
+  return (
+    <div>
+      <span className={Load.loader2}></span>
+    </div>
+  );
+};
+
+export default Loading;
--- app/admin/user/page.tsx
+++ app/admin/user/page.tsx
@@ -8,10 +8,13 @@ import AdminStyle from "@/app/admin/admin.module.scss";
 import Search from "../(component)/Search";
 import AdminApi from "@/app/(api)/admin/adminApi";
 import Paging from "../(component)/pagenation";
-// import SideBar from "../sidebar";
-// import SideBar from "../(component)/sideBar";
+import "./../(component)/Paging.scss";
+import Pagination from "react-js-pagination";
+import { LeftOutlined, RightOutlined } from "@ant-design/icons";
+import Loading from "@/app/admin/(component)/Loading";
 
 interface UserData {
+  memberId: string;
   reviewId: number;
   email: string;
   isDeleted: boolean;
@@ -21,19 +24,25 @@ interface UserData {
 
 const userPage = () => {
   const [page, setPage] = useState(1);
+  const [totalPages, setTotalPages] = useState(1);
   const [userCheck, setUserCheck] = useState<Array<UserData>>([]);
   const [loading, setLoading] = useState(true);
   const { getuserAll } = AdminApi();
 
   useEffect(() => {
     fetchUserCheck();
-  }, []);
+  }, [page, totalPages]);
 
   const fetchUserCheck = async () => {
-    const result = await getuserAll();
+    const result = await getuserAll(page - 1);
     setUserCheck(result.content);
+    setTotalPages(result.totalElements);
     setLoading(false);
-    console.log(result.data);
+  };
+
+  const handlePageChange = (page: React.SetStateAction<number>) => {
+    setPage(page);
+    console.log(page);
   };
 
   // const handlePageChange = (page: React.SetStateAction<number>) => {
@@ -47,26 +56,42 @@ const userPage = () => {
         <p className={AdminStyle.pStyle}>사용자 목록</p>
         <Search />
       </div>
-      <table className={AdminStyle.tableStyle}>
-        <thead>
-          <tr className={AdminStyle.trStyle}>
-            <th className={AdminStyle.nickname}>닉네임</th>
-            <th className={AdminStyle.reviewContent}>이메일</th>
-            <th className={AdminStyle.reportContent}>연락처</th>
-            <th className={AdminStyle.date}>상태</th>
-          </tr>
-        </thead>
-        <tbody>
-          {userCheck.map((userData, index) => (
-            <UserList
-              key={index}
-              userData={userData}
-              // removeMember={removeMember}
-            />
-          ))}
-        </tbody>
-      </table>
-      {!loading && <Paging />}
+      {loading ? (
+        <div className={AdminStyle.load}>
+          <Loading />{" "}
+        </div>
+      ) : (
+        <>
+          <table className={AdminStyle.tableStyle}>
+            <thead>
+              <tr className={AdminStyle.trStyle}>
+                <th className={AdminStyle.nickname}>닉네임</th>
+                <th className={AdminStyle.reviewContent}>이메일</th>
+                <th className={AdminStyle.reportContent}>연락처</th>
+                <th className={AdminStyle.date}>상태</th>
+              </tr>
+            </thead>
+            <tbody>
+              {/* {userCheck.map((userData, index) => (
+            <UserList key={index} userData={userData} />
+          ))} */}
+              {userCheck.map((userData: UserData) => (
+                <UserList key={userData.memberId} userData={userData} />
+              ))}
+            </tbody>
+          </table>
+
+          <Pagination
+            activePage={page}
+            itemsCountPerPage={5}
+            totalItemsCount={totalPages}
+            pageRangeDisplayed={5}
+            prevPageText={<LeftOutlined />}
+            nextPageText={<RightOutlined />}
+            onChange={handlePageChange}
+          />
+        </>
+      )}
     </div>
   );
 };
