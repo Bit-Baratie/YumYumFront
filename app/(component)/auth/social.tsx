11f67a9bd103281a17ddcff67aaed5f13a798aa3
dyddks <dyddks0115@naver.com>
2024-07-17 18:40:46 +0900

    chore: 소셜로그인 url 수정

--- app/(component)/auth/social.tsx
+++ app/(component)/auth/social.tsx
@@ -1,7 +1,7 @@
 import Naver from '@/public/asset/image/naver.svg';
 
 export const NaverLogin = () => {
-  const NAVER_AUTH_URL = `http://192.168.0.20:3000/oauth2/authorization/naver`;
+  const NAVER_AUTH_URL = `http://223.130.158.171:80/oauth2/authorization/naver`;
 
   const naverLogin = () => {
     window.location.href = NAVER_AUTH_URL;
