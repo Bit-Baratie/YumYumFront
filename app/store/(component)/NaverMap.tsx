b122e65f44bfc24fbc28635e48ab87e23f039643
cyj1127 <157556973+cyj1127@users.noreply.github.com>
2024-07-19 12:17:27 +0900

    perf:맛집상세 지도랜더링 오류 해결

--- app/store/(component)/NaverMap.tsx
+++ app/store/(component)/NaverMap.tsx
@@ -4,14 +4,17 @@ import { useEffect, useRef } from "react";
 import { getStoreType, location } from "@/app/type";
 import Script from "next/script";
 import useSearch from "@/app/(hooks)/common/useSearch";
+import { useRouter } from "next/navigation";
 
 const TestMap = ({
+
   storeInfo,
   myLatLng,
 }: {
   storeInfo: getStoreType[];
   myLatLng: location | null;
 }) => {
+  const router = useRouter();
   const mapRef = useRef<HTMLDivElement>(null);
   let map: naver.maps.Map | null = null;
   const { data } = useSearch();
@@ -20,73 +23,82 @@ const TestMap = ({
     const { naver } = window;
     if (!mapRef.current || !naver) return;
 
-    console.log(storeInfo);
-    if (storeInfo?.length > 1) {
-      map = new naver.maps.Map("map", {
-        center: new naver.maps.LatLng(myLatLng?.latitude, myLatLng?.longitude),
-        zoomControl: true,
-        zoom: 15,
-        minZoom: 6,
-        zoomControlOptions: {
-          style: naver.maps.ZoomControlStyle.LARGE,
-          position: naver.maps.Position.TOP_RIGHT,
-        },
-      });
-    } else if (storeInfo?.length == 1) {
-      map = new naver.maps.Map("map", {
-        center: new naver.maps.LatLng(
-          storeInfo[0]?.latitude,
-          storeInfo[0]?.longitude
-        ),
-        zoomControl: true,
-        zoom: 15,
-        minZoom: 6,
-        zoomControlOptions: {
-          style: naver.maps.ZoomControlStyle.LARGE,
-          position: naver.maps.Position.TOP_RIGHT,
-        },
-      });
+    try {
+      console.log(storeInfo);
+      if (storeInfo?.length > 1) {
+        map = new naver.maps.Map("map", {
+          center: new naver.maps.LatLng(myLatLng?.latitude, myLatLng?.longitude),
+          zoomControl: true,
+          zoom: 15,
+          minZoom: 6,
+          zoomControlOptions: {
+            style: naver.maps.ZoomControlStyle.LARGE,
+            position: naver.maps.Position.TOP_RIGHT,
+          },
+        });
+      } else if (storeInfo?.length == 1) {
+        map = new naver.maps.Map("map", {
+          center: new naver.maps.LatLng(
+            storeInfo[0]?.latitude,
+            storeInfo[0]?.longitude
+          ),
+          zoomControl: true,
+          zoom: 15,
+          minZoom: 6,
+          zoomControlOptions: {
+            style: naver.maps.ZoomControlStyle.LARGE,
+            position: naver.maps.Position.TOP_RIGHT,
+          },
+        });
+      }
+    } catch (err) {
+      console.error(err);
+      router.refresh();
     }
 
-    storeInfo?.map((store: getStoreType) => {
-      const contentString = [
-        "<div class='infowindow-content'>",
-        `   <h3>${store.name}</h3>`,
-        `   <div>★${store.avgGrade}(${store.totalReviewCount})&nbsp;♥️${store.totalFavoriteCount}</div>`,
-        `   <p>${store.address}<br />`,
-        `   <div>${store.categoryList ? store.categoryList : ""}</div></p>`,
-        "</div>",
-      ].join("");
+    try {
+      storeInfo?.map((store: getStoreType) => {
+        const contentString = [
+          "<div class='infowindow-content'>",
+          `   <h3>${store.name}</h3>`,
+          `   <div>★${store.avgGrade}(${store.totalReviewCount})&nbsp;♥️${store.totalFavoriteCount}</div>`,
+          `   <p>${store.address}<br />`,
+          `   <div>${store.categoryList ? store.categoryList : ""}</div></p>`,
+          "</div>",
+        ].join("");
 
-      const infowindow = new naver.maps.InfoWindow({
-        content: contentString,
-        maxWidth: 300,
-        backgroundColor: "#FFFaEF",
-        borderWidth: 0,
-        anchorColor: "white",
-        anchorSkew: true,
+        const infowindow = new naver.maps.InfoWindow({
+          content: contentString,
+          maxWidth: 300,
+          backgroundColor: "#FFFaEF",
+          borderWidth: 0,
+          anchorColor: "white",
+          anchorSkew: true,
+        });
+
+        if (map) {
+          let marker = new naver.maps.Marker({
+            position: new naver.maps.LatLng(store.latitude, store.longitude),
+            map: map,
+            title: store.name,
+          });
+
+          naver.maps.Event.addListener(marker, "click", function () {
+            if (infowindow.getMap()) {
+              infowindow.close();
+            } else {
+              infowindow.open(map!, marker);
+            }
+          });
+
+          infowindow.open(map!, marker);
+        }
       });
-
-      if (map) {
-        let marker = new naver.maps.Marker({
-          position: new naver.maps.LatLng(store.latitude, store.longitude),
-          map: map,
-          title: store.name,
-        });
-
-        naver.maps.Event.addListener(marker, "click", function () {
-          if (infowindow.getMap()) {
-            infowindow.close();
-          } else {
-            infowindow.open(map!, marker);
-          }
-        });
-
-        infowindow.open(map!, marker);
-      }
-    });
-  };
-
+    } catch (err) {
+      console.error(err);
+      router.refresh();
+    }
+  }
   useEffect(() => {
     initializeMap();
   }, [storeInfo]);
