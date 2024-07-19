6781cbf5bbddcd3c8a5c634f9ab6461e56ecb2ab
dyddks <dyddks0115@naver.com>
2024-07-19 11:20:05 +0900

    chore: 배너이미지 최적화

--- app/home/page.tsx
+++ app/home/page.tsx
@@ -5,7 +5,7 @@ import PageStyle from './page.module.scss';
 const page = () => {
   return (
     <>
-      <Image src={'/asset/image/banner.png'} alt="배너이미지" width={0} height={0} sizes='100vw' className={PageStyle.img}/>
+      <Image src={'/asset/image/banner.png'} alt="배너이미지" width={0} height={0} sizes='100vw' className={PageStyle.img} priority={true}/>
       <Slide/>
     </>
   );
