c7e3fafc49604912d45adb99e9c8060ff01f148a
kimsiun <157556686+kimsiun@users.noreply.github.com>
2024-07-19 18:17:44 +0900

    feat: 날짜 표시 라이브러리 추가

--- app/(component)/reply/comment.tsx
+++ app/(component)/reply/comment.tsx
@@ -3,39 +3,69 @@ import { getReplyType } from "../../type";
 import useModal from "../../(hooks)/common/useModal";
 import ReportModal from "../reportModal";
 import CustomImage from "../common/customImage";
+import moment from "moment";
+import "moment/locale/ko";
 
-const Comment = ({item, updateReply, removeReply}: {item: getReplyType, updateReply: Function, removeReply: Function}) => {
-    const {modal, setModal, createReplyReport, contentHandler, content} = useModal();
+const Comment = ({
+  item,
+  updateReply,
+  removeReply,
+}: {
+  item: getReplyType;
+  updateReply: Function;
+  removeReply: Function;
+}) => {
+  const { modal, setModal, createReplyReport, contentHandler, content } =
+    useModal();
 
-    const onClose = () => {
-        setModal(false);
-    }
+  const onClose = () => {
+    setModal(false);
+  };
 
-    return (
-        <>
-            <div className="commentContainer">
-            <div className="content">
-                <CustomImage style={'comment-img'} src={item.imageUrl} alt="프로필 이미지" width={30} height={30}/>
+  return (
+    <>
+      <div className="commentContainer">
+        <div className="content">
+          <CustomImage
+            style={"comment-img"}
+            src={item.imageUrl}
+            alt="프로필 이미지"
+            width={30}
+            height={30}
+          />
 
-                <div className="right">
-                <p className="profile-name">{item.nickname}</p>
-                <p>{item.content}</p>
-                <p>{item.createdAt}</p>
-                </div>
-            </div>
+          <div className="right">
+            <p className="profile-name">{item.nickname}</p>
+            <p className="ReplyCont">{item.content}</p>
+            <p className="ReplyDate">
+              {moment(item.createdAt).format("MM월 DD일 a hh:mm ")}
+            </p>
+          </div>
+        </div>
 
-            <div className="btn">
-                <button className="dec" onClick={() => setModal(true)}></button>
-                <button className="mod" onClick={() => updateReply()}></button>
-                <button className="del" onClick={() => removeReply(item.replyId)}></button>
-            </div>
-            </div>
+        <div className="btn">
+          <button className="dec" onClick={() => setModal(true)}></button>
+          <button className="mod" onClick={() => updateReply()}></button>
+          <button
+            className="del"
+            onClick={() => removeReply(item.replyId)}
+          ></button>
+        </div>
+      </div>
 
-            <hr />
+      <hr />
 
-            {modal && <ReportModal onClose={() => onClose()} targetId={item.replyId} createReport={createReplyReport} content={content} contentHandler={contentHandler}/>}
-        </>
-    );
-}
+      {modal && (
+        <ReportModal
+          onClose={() => onClose()}
+          targetId={item.replyId}
+          createReport={createReplyReport}
+          content={content}
+          contentHandler={contentHandler}
+        />
+      )}
+    </>
+  );
+};
 
 export default Comment;
