c592604b67687cd22cc18f10be56a21e6e1e222e
dyddks <dyddks0115@naver.com>
2024-07-22 14:43:04 +0900

    fix: 로그인 예외처리 수정

--- app/(api)/member/loginApi.ts
+++ app/(api)/member/loginApi.ts
@@ -2,8 +2,15 @@ import {axiosNonAuth} from "@/app/(hooks)/common/axiosWithAuth";
 import { loginType } from "@/app/type";
 
 const postLoginInfo = async (loginInfo: loginType) => {
-  const result = await axiosNonAuth.post(`/member/login`, loginInfo);
-  return result.data;
+  const result = await axiosNonAuth.post(`/member/login`, loginInfo)
+  .then((res) => {
+    return res.data;
+  })
+  .catch((err) => {
+    alert('이메일 혹은 비밀번호를 확인해주세요');
+  });
+
+  return result;
 }
 
 export default postLoginInfo;
