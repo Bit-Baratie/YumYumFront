b05cdd1c0e1c5eeec3938f6ede160c5bce99cb2d
cyj1127 <157556973+cyj1127@users.noreply.github.com>
2024-07-22 12:13:02 +0900

    fix:맛집상세 css 수정 및 axios ip 수정

--- app/(hooks)/common/axiosWithAuth.ts
+++ app/(hooks)/common/axiosWithAuth.ts
@@ -5,7 +5,7 @@ import { redirect } from 'next/navigation';
 
 
 export const axiosWithAuth: AxiosInstance = axios.create({
-  baseURL: 'http://192.168.0.57:3000',
+  baseURL: 'http://223.130.139.146:80',
   headers: {
     Authorization: `Bearer ${Store.getState().token.atk}`,
     "Content-Type": 'application/json'
@@ -14,7 +14,7 @@ export const axiosWithAuth: AxiosInstance = axios.create({
 });
 
 export const axiosNonAuth: AxiosInstance = axios.create({
-  baseURL: 'http://192.168.0.57:3000',
+  baseURL: 'http://223.130.139.146:80',
   headers: {
     Authorization: `Bearer ${Store.getState().token.atk}`,
     "Content-Type": 'application/json'
