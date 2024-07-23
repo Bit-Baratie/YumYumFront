624ce61f689d0132a26da13e3964fb50631a4661
dyddks <dyddks0115@naver.com>
2024-07-23 14:01:37 +0900

    refactor: login useMutation으로 리팩토링

--- app/(api)/member/loginApi.ts
+++ app/(api)/member/loginApi.ts
@@ -5,9 +5,6 @@ const postLoginInfo = async (loginInfo: loginType) => {
   const result = await axiosNonAuth.post(`/member/login`, loginInfo)
   .then((res) => {
     return res.data;
-  })
-  .catch((err) => {
-    alert('이메일 혹은 비밀번호를 확인해주세요');
   });
 
   return result;
