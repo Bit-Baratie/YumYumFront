9690cd64a5aaeeef4806dbbb3830b35b27f49d7f
kimsiun <157556686+kimsiun@users.noreply.github.com>
2024-07-23 11:44:31 +0900

    fix: 마이페이지 디자인 수정

--- app/(component)/member/updateModal.tsx
+++ app/(component)/member/updateModal.tsx
@@ -1,8 +1,9 @@
-import Image from 'next/image';
-import ModalStyle from './updateModal.module.scss';
-import { ChangeEvent, useEffect } from 'react';
-import useMember from '@/app/(hooks)/member/useMember';
-import CustomImage from '../common/customImage';
+import Image from "next/image";
+import ModalStyle from "./updateModal.module.scss";
+import { ChangeEvent, useEffect } from "react";
+import useMember from "@/app/(hooks)/member/useMember";
+import CustomImage from "../common/customImage";
+import Pencil from "@/public/asset/image/pencil.png";
 
 interface Props {
   imageHandler: (e: ChangeEvent<HTMLInputElement>) => void;
@@ -15,11 +16,11 @@ interface Props {
   imageUrl: any;
   fileInput: any;
   modal: boolean;
-  setModal: (i:boolean) => void;
+  setModal: (i: boolean) => void;
 }
 
-const UpdateModal = (
-  {imageHandler,
+const UpdateModal = ({
+  imageHandler,
   nickNameHandler,
   passwordHanler,
   passwordCheckHanler,
@@ -32,63 +33,130 @@ const UpdateModal = (
   setModal,
   setImageUrl,
   setPhone,
-  setNickName
-}: any
-) => {
-
+  setNickName,
+}: any) => {
   useEffect(() => {
     setImageUrl(profile.imageUrl);
     setNickName(profile.nickname);
     setPhone(profile.phoneNumber);
-  }, [])
+  }, []);
 
   return (
     <>
       <div className={ModalStyle.background}></div>
 
       <div className={ModalStyle.container}>
-        <div className={ModalStyle.esc} onClick={() => setModal(false)}>BC</div>
+        <div className={ModalStyle.esc} onClick={() => setModal(false)}>
+          <div className={ModalStyle.X}>X</div>
+        </div>
         <span className={ModalStyle.title}>프로필 수정</span>
-        <CustomImage style={ModalStyle.img} src={imageUrl} width={100} height={100} alt='프로필'/>
-        <label htmlFor="input-file" >Profile Image</label>
-          <input type="file" id='input-file' placeholder='Profile Image' ref={fileInput} onChange={(e) => imageHandler(e)} style={{display:'none'}} />
+        <CustomImage
+          style={ModalStyle.img}
+          src={imageUrl}
+          width={100}
+          height={100}
+          alt="프로필"
+        />
+        <label htmlFor="input-file">Profile Image</label>
+        <input
+          type="file"
+          id="input-file"
+          placeholder="Profile Image"
+          ref={fileInput}
+          onChange={(e) => imageHandler(e)}
+          style={{ display: "none" }}
+        />
         {/* <div className={ModalStyle.img}></div> */}
 
-        <div>
-          <input type="text" defaultValue={profile.nickname} className={ModalStyle.nickname} onChange={(e) => nickNameHandler(e)}/>
-          <span>연필</span>
+        <div className={ModalStyle.nameMod}>
+          <input
+            type="text"
+            defaultValue={profile.nickname}
+            className={ModalStyle.nickname}
+            onChange={(e) => nickNameHandler(e)}
+          />
+          <span>
+            <Image
+              src={"/asset/image/pencil.png"}
+              width={15}
+              height={15}
+              alt="연필"
+            />
+          </span>
         </div>
 
         <div>
           <div className={ModalStyle.inputSet}>
             <span className={ModalStyle.label}>연락처</span>
             <div className={ModalStyle.inputBox}>
-              <input type="text" defaultValue={profile.phoneNumber} className={ModalStyle.input} onChange={(e) => phoneHandler(e)}/>
-              <span>연필</span>
+              <input
+                type="text"
+                defaultValue={profile.phoneNumber}
+                className={ModalStyle.input}
+                onChange={(e) => phoneHandler(e)}
+              />
+              <span>
+                <Image
+                  src={"/asset/image/pencil.png"}
+                  width={15}
+                  height={15}
+                  alt="연필"
+                />
+              </span>
             </div>
           </div>
 
           <div className={ModalStyle.inputSet}>
             <span className={ModalStyle.label}>변경 비밀번호</span>
             <div className={ModalStyle.inputBox}>
-              <input type="password" className={ModalStyle.input} onChange={(e) => passwordHanler(e)}/>
-              <span>연필</span>
+              <input
+                type="password"
+                className={ModalStyle.input}
+                onChange={(e) => passwordHanler(e)}
+              />
+              <span>
+                <Image
+                  src={"/asset/image/pencil.png"}
+                  width={15}
+                  height={15}
+                  alt="연필"
+                />
+              </span>
             </div>
           </div>
 
           <div className={ModalStyle.inputSet}>
             <span className={ModalStyle.label}>변경 비밀번호 확인</span>
             <div className={ModalStyle.inputBox}>
-              <input type="password" className={ModalStyle.input} onChange={(e) => passwordCheckHanler(e)}/>
-              <span>연필</span>
+              <input
+                type="password"
+                className={ModalStyle.input}
+                onChange={(e) => passwordCheckHanler(e)}
+              />
+              <span>
+                <Image
+                  src={"/asset/image/pencil.png"}
+                  width={15}
+                  height={15}
+                  alt="연필"
+                />
+              </span>
             </div>
           </div>
         </div>
 
-        <button className={ModalStyle.submit} onClick={() => {updateMember(); setModal(false)}}>확인</button>
+        <button
+          className={ModalStyle.submit}
+          onClick={() => {
+            updateMember();
+            setModal(false);
+          }}
+        >
+          확인
+        </button>
       </div>
     </>
   );
-}
+};
 
 export default UpdateModal;
