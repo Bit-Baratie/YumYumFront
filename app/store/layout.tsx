c08f3abdf9a52e463c6f2808819d7acdd09a3252
cyj1127 <157556973+cyj1127@users.noreply.github.com>
2024-07-24 17:47:11 +0900

    style:반응형 scss관련 파일 수정 및 맛집리스트 반응형 scss

--- app/store/layout.tsx
+++ app/store/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import Header from "../header";
 import Script from "next/script";
+import detailScss from "@/app/store/[store_id]/storeDetailPage.module.scss";
 
 export const metadata: Metadata = {
   title: "YumYum 맛집 리스트",
@@ -20,7 +21,7 @@ export default function RootLayout({
   return (
     <>
       {/* <Script strategy='beforeInteractive' src="https://openapi.map.naver.com/openapi/v3/maps.js?ncpClientId=nqz0uvnoe5&callback=Callback" /> */}
-      <Header />
+      <div className={detailScss.mobileHeader}><Header /></div>
       <div>
         {children}
       </div>
