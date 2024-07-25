a1e2b1b110872adda001386ed75e8bcb16cb5f96
cyj1127 <157556973+cyj1127@users.noreply.github.com>
2024-07-25 19:50:11 +0900

    fix:마이페이지 좋아요 한 맛집 import 수정

--- app/member/[member_id]/star/page.tsx
+++ app/member/[member_id]/star/page.tsx
@@ -1,5 +1,5 @@
 'use client'
-import StoreInfo from "@/app/store/(component)/StoreInfo";
+import StoreInfo from "@/app/(component)/store/StoreInfo";
 import PageStyle from './page.module.scss';
 import useMember from "@/app/(hooks)/member/useMember";
 import { useObserver } from "@/app/(hooks)/common/useObserver";
@@ -7,22 +7,22 @@ import { useRef } from "react";
 import { getStoreType } from "@/app/type";
 
 const StarPage = () => {
-  const {likeStoreList, nextLikeStoreList} = useMember();
+  const { likeStoreList, nextLikeStoreList } = useMember();
   const bottomRef = useRef(null);
-  const onIntersect = ([entry]:any) => entry.isIntersecting && nextLikeStoreList();
+  const onIntersect = ([entry]: any) => entry.isIntersecting && nextLikeStoreList();
 
   useObserver({
     target: bottomRef,
     onIntersect
   });
-  
+
   return (
     <>
       <div className={PageStyle.container}>
         {likeStoreList?.pages.map((page) => (
           <div key={page}>
             {page.content.map((item: getStoreType) => (
-              <StoreInfo store={item} key={item.storeId}/>
+              <StoreInfo store={item} key={item.storeId} />
             ))}
           </div>
         ))}
