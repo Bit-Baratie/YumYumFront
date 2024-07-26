2455b1bbc35c061d971c1c2ddb140b17ae34c7db
dyddks <dyddks0115@naver.com>
2024-07-26 11:02:02 +0900

    chore: 소셜로그인 요청주소 수정

--- app/(component)/auth/social.tsx
+++ app/(component)/auth/social.tsx
@@ -2,7 +2,7 @@ import Naver from '@/public/asset/image/naver.svg';
 import Kakao from '@/public/asset/image/Kakao.svg';
 
 export const NaverLogin = () => {
-  const NAVER_AUTH_URL = `http://223.130.158.171:80/oauth2/authorization/naver`;
+  const NAVER_AUTH_URL = `https://api.baratie.site/oauth2/authorization/naver`;
 
   const naverLogin = () => {
     window.location.href = NAVER_AUTH_URL;
@@ -14,7 +14,7 @@ export const NaverLogin = () => {
 }
 
 export const KakaoLogin = () => {
-  const NAVER_AUTH_URL = `http://192.168.0.20:3000/oauth2/authorization/kakao`;
+  const NAVER_AUTH_URL = `https://api.baratie.site/oauth2/authorization/kakao`;
 
   const naverLogin = () => {
     window.location.href = NAVER_AUTH_URL;
