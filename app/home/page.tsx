36e23fb258204d395dfee60d0dce11e97f1b8119
dyddks <dyddks0115@naver.com>
2024-07-26 10:57:07 +0900

    chore: 메인페이지 레이아웃 수정

--- app/home/page.tsx
+++ app/home/page.tsx
@@ -5,10 +5,10 @@ import bannerImg from '@/public/asset/image/banner.png';
 
 const page = () => {
   return (
-    <>
+    <div className={PageStyle.container}>
       <Image src={bannerImg} alt="배너이미지" width={0} height={0} sizes='100vw' className={PageStyle.img} priority={true} placeholder="blur"/>
       <Slide/>
-    </>
+    </div>
   );
 }
 
