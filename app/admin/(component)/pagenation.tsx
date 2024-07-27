2dca2c08a51e8e0b435b717c102c7c4c44a46fb3
kimsiun <157556686+kimsiun@users.noreply.github.com>
2024-07-27 15:45:52 +0900

    fix: 페이지네이션 디자인 수정

--- app/admin/(component)/pagenation.tsx
+++ app/admin/(component)/pagenation.tsx
@@ -1,7 +1,7 @@
 import React, { useState } from "react";
 import "./Paging.scss";
 import Pagination from "react-js-pagination";
-import { LeftOutlined, RightOutlined } from "@ant-design/icons";
+import { CaretLeftFilled, CaretRightFilled } from "@ant-design/icons";
 
 const Paging = () => {
   const [page, setPage] = useState(1);
@@ -17,8 +17,8 @@ const Paging = () => {
       itemsCountPerPage={10} // 한 페이지당 보여줄 리스트 아이템 개수
       totalItemsCount={450} // 총 아이템의 개수
       pageRangeDisplayed={5} // Pagination 내에서 보여줄 페이지의 범위
-      prevPageText={<LeftOutlined />}
-      nextPageText={<RightOutlined />}
+      prevPageText={<CaretLeftFilled />}
+      nextPageText={<CaretRightFilled />}
       onChange={handlePageChange} // 페이지가 바뀔 때 핸들링해줄 함수
     />
   );
