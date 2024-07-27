11c82e587f650b7d05d04dd68e1f74c1b12bac7d
kimsiun <157556686+kimsiun@users.noreply.github.com>
2024-07-27 15:42:32 +0900

    fix: 메인화면 아이템 컴포넌트 수정

--- app/(component)/home/item.tsx
+++ app/(component)/home/item.tsx
@@ -5,20 +5,29 @@ import Link from "next/link";
 import useImage from "@/app/(hooks)/common/useImage";
 import { StarFilled, HeartFilled } from "@ant-design/icons";
 import CustomImage from "../common/customImage";
+import Bookmark from "@/public/asset/image/mark.svg";
 
 const Item = ({ item }: { item: getStoreListType }) => {
   return (
     <Link href={`/store/${item.storeId}`} className={ItemStyle.container}>
-      <CustomImage style={''} src={item.imageUrl} width={180} height={180} alt="가게이미지"/>
+      <CustomImage
+        style={""}
+        src={item.imageUrl}
+        width={180}
+        height={180}
+        alt="가게이미지"
+      />
       <div className={ItemStyle.name}>{item.name}</div>
       <div className={ItemStyle.iconWrapper}>
         <div className={ItemStyle.iconText}>
           <StarFilled className={ItemStyle.icon} />
-          <span>{item.avgGrade}({item.reviewCount})</span>
+          <span>
+            {item.avgGrade}({item.reviewCount})
+          </span>
         </div>
-
+        &ensp;
         <div className={ItemStyle.iconText}>
-          <HeartFilled className={ItemStyle.icon} />
+          <Bookmark className={ItemStyle.icon} width={16} height={16} />
           <span>{item.favoriteCount}</span>
         </div>
       </div>
