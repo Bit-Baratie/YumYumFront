6bf851987965d0ad02559dc8bc01afe5db418f80
dyddks <dyddks0115@naver.com>
2024-07-27 22:01:53 +0900

    chore: 헤더 css 수정

--- app/home/page.tsx
+++ app/home/page.tsx
@@ -6,7 +6,7 @@ import bannerImg from '@/public/asset/image/banner.png';
 const page = () => {
   return (
     <div className={PageStyle.container}>
-      <Image src={bannerImg} alt="배너이미지" width={0} height={0} sizes='100vw' className={PageStyle.img} priority={true} placeholder="blur"/>
+      <Image src={bannerImg} alt="배너이미지" width={0} height={0} sizes="100%" className={PageStyle.img} priority={true} placeholder="blur"/>
       <Slide/>
     </div>
   );
