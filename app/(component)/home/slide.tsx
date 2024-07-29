646ef287979ab6d0d95cc6c4671aea6c5be60394
dyddks <dyddks0115@naver.com>
2024-07-29 16:08:08 +0900

    chore: 드롭박스 아이템 수정

--- app/(component)/home/slide.tsx
+++ app/(component)/home/slide.tsx
@@ -35,18 +35,18 @@ const Slide = () => {
           <option value="서울">서울</option>
           <option value="경기">경기</option>
           <option value="인천">인천</option>
-          <option value="충북">충북</option>
-          <option value="충남">충남</option>
+          <option value="충북">충청북도</option>
+          <option value="충남">충청남도</option>
           <option value="대전">대전</option>
           <option value="세종">세종</option>
-          <option value="전북">전북</option>
-          <option value="전남">전남</option>
+          <option value="전북">전라북도</option>
+          <option value="전남">전라남도</option>
           <option value="광주">광주</option>
-          <option value="경북">경북</option>
-          <option value="경남">경남</option>
+          <option value="경북">경상북도</option>
+          <option value="경남">경상남도</option>
           <option value="부산">부산</option>
           <option value="울산">울산</option>
-          <option value="강원">강원</option>
+          <option value="강원">강원도</option>
           <option value="제주">제주</option>
         </select>
 
